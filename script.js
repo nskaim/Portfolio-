194590ea5c1c8208d3ff4e440e9fb465c5a5ee39
SUNIL KUMAR <112271482+nskaim@users.noreply.github.com>
2024-12-31 19:19:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menu=document.getElementById('hamburger');
-const list=document.getElementById('list');
-
-menu.addEventListener("click" , () => {
-    list.classList.toggle('navlist-active');
-
-})
